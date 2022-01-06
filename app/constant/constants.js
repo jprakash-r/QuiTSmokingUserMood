294b6bb492b0c62e1ca1881ebdcd37689b518bd3
jprakash-r <jprakash.r@outlook.com>
2022-01-07 00:45:32 +0530

    QuitSmoking - initial commit

--- app/constant/constants.js
+++ app/constant/constants.js
@@ -1,6 +1,11 @@
  var constants = {
     USER_MOOD_HAPPY : "happy",
-    USER_MOOD_SAD : "sad"
+    USER_MOOD_SAD : "sad",
+    USER_MOOD_BAD : "bad",
+    USER_MOOD_SURPRISED : "surprised",
+    USER_MOOD_FEARFUL : "fearful",
+    USER_MOOD_ANGRY : "angy",
+    USER_MOOD_DISGUSTED : "disgusted"
 }
 
 module.exports.constants = constants
